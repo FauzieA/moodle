33c031d9959520bb1a27af09bb44664ccdcec769
Fauziyya Abdullahi Ahmed <111000780+FauzieA@users.noreply.github.com>
2025-05-13 04:20:31 +0800

    Delete local/moowoodle directory

--- local/moowoodle/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-element', 'wp-i18n'), 'version' => 'b929a4c0350337fedc3f');
--- local/moowoodle/src/components/AdminLibrary/Inputs/Special/RegistrationForm/Templates/Divider.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Divider = () => {
-  return (
-    <div className='section-divider-container'>Section Divider</div>
-  )
-}
-
-export default Divider
