ce7a84925988b54053f7057a9a0b23e4c87eb279
Fauziyya Abdullahi Ahmed <111000780+FauzieA@users.noreply.github.com>
2025-05-15 03:17:30 +0800

    Create version.php

---
+++ local/moowoodle/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_moowoodle';
+$plugin->version   = 2024051300; // Update this if needed
+$plugin->requires  = 2021051700; // Moodle 3.11 or later
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0';
